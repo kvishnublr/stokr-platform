2148994243719b06925856dc4124450954a61227
root <root@vmi3311695.contaboserver.net>
2026-06-16 04:03:24 +0200

    fix: add strategyCatalog stub for SymbolMultiSelect component

---
+++ stokr-ui/src/api/strategyCatalog.ts
@@ -0,0 +1,7 @@
+export async function searchSymbols(_query: string, _limit?: number): Promise<string[]> {
+  return [];
+}
+
+export async function fetchCatalog(): Promise<unknown[]> {
+  return [];
+}
