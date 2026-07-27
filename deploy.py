b2ff4e984ba276f2f43e2b74ab9e512dad48db71
itsvi <itsvi@users.noreply.local>
2026-07-27 22:16:52 +0530

    fix: force UTF-8 stdout in deploy script

--- deploy.py
+++ deploy.py
@@ -3,6 +3,10 @@
 import paramiko
 import os
 import sys
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8', errors='replace')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8', errors='replace')
 
 HOST = "173.249.55.84"
 USER = "root"
